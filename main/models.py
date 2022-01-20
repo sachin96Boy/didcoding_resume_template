5a0c0c89fc301ae2c7177b014b40ab20d04dc0c2
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-01-20 19:58:50 +0530

    configure models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,71 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
+
+class Skill(models.Model):
+    class Meta:
+        verbose_name_plural = 'skills'
+        verbose_name = 'skill'
+    name = models.CharField(max_length=20, blank=True, null=True)
+    score = models.IntegerField(default=80, blank=True, null=True)
+    image = models.FileField(blank=True, null=True, upload_to="skills")
+    is_key_skill = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'User Profiles'
+        verbose_name = 'User_Profile'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    avatar = models.ImageField(blank="True", null="True", upload_to="avatar")
+    title = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True) 
+    skills = models.ManyToManyField(Skill, blank = True)
+    cv = models.FileField(blank=True, null=True, upload_to="cv")
+
+    def _str_(self):
+        return f'{self.user.first_name} {self.user.last_name}'   
+
+
+class ContactProfile(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Contact Profiles"
+        verbose_name = "Contact Profile"
+        ordering= ["timestamp"]
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(verbose_name="Name", max_length=100)
+    email = models.EmailField(verbose_name="Email")
+    message =models.TextField(verbose_name="Message")
+
+    def _str_(self):
+        return f'{self.name}'
+
+
+class Testimonial(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Testimonials"
+        vrbose_name = "Testimonial"
+        ordering = ["name"]
+
+    thumbnail = models.ImageField(blank=True, null=True, upload_to="testimonials")
+    name = models.CharField(max_length=200, blank=True, null=True)
+    role = models.CharField(max_length=200, blank=True, null=True)
+    quote = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default = True)
+
+    def _str_(self):
+        return self.name
+
+
 
 # Create your models here.
